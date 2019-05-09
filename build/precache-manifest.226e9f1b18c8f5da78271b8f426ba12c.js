171aab019ed4fec52e5b868b69c3e07c06629553
Ifeanyi Osinakayah Joseph <ifeanyi.Osinakayah@cousant.com>
2019-05-09 10:26:30 +0100

    First commit

---
+++ build/precache-manifest.226e9f1b18c8f5da78271b8f426ba12c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5187ed1cc89ceb6e148de34b7179e4c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4448c2fadcf09d109370",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "792c49069da0ec6edbcb",
+    "url": "/static/js/2.e006d419.chunk.js"
+  },
+  {
+    "revision": "4448c2fadcf09d109370",
+    "url": "/static/js/main.ce4f657e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
