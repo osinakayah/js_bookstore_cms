0f38f0032c154f1afee80f1b6fd34e8be71afbda
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2019-05-09 13:42:49 +0200

    Logic part project finished

---
+++ src/logic/book.js
@@ -0,0 +1,10 @@
+export const Book = (id, title, category) => {
+
+    return {
+        id,
+        title,
+        category
+    }
+}
+
+export const BOOK_CATEGORIES =  ["Action", "Biography", "History", "Horror", "Kids", "Learning", "Sci-Fi"]
