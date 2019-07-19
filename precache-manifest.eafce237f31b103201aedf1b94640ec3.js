7bfed41017b5546734f5246d10b4febc6dad57fc
Ifeanyi Osinakayah Joseph <ifeanyi.Osinakayah@cousant.com>
2019-07-19 12:41:28 +0100

    Updates

---
+++ precache-manifest.eafce237f31b103201aedf1b94640ec3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c117b6ccc7de5339e374eb87e86f5a65",
+    "url": "/js_bookstore_cms/index.html"
+  },
+  {
+    "revision": "725e6a60cf6f42f7e9ec",
+    "url": "/js_bookstore_cms/static/css/main.33a5ce08.chunk.css"
+  },
+  {
+    "revision": "46a1ae8fc1f5dcc02789",
+    "url": "/js_bookstore_cms/static/js/2.136dc67e.chunk.js"
+  },
+  {
+    "revision": "725e6a60cf6f42f7e9ec",
+    "url": "/js_bookstore_cms/static/js/main.343fef11.chunk.js"
+  },
+  {
+    "revision": "4859d8ece2dd13035461",
+    "url": "/js_bookstore_cms/static/js/runtime~main.ce7da786.js"
+  }
+]);
